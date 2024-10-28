5a59d4971f02107a9d76f20333ce5fb7ee986d43
cruzrafaelx <rafaelcruz@outlook.de>
2024-10-28 01:31:24 +0100

    add about page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import image55 from './bgImages/image55.png'
 
 function Home(){
   return(
-    <div>
+    <div className='home'>
       <Header/>
 
       <div className='hero-container' style={{
@@ -26,11 +26,22 @@ function Home(){
 
 function About(){
   return(
-    <div>
+    <div className='about'>
       <Header/>
+      <img src={image55} 
+             class='about-img'
+             alt='A boy sitting on a van looking up the stars'>
+        </img>
 
-      <div>
-        <img src={image55} alt='A boy sitting on a van looking up the stars'></img>
+      <div className='about-container'>
+        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.(Hitch costs extra 😉)</p>
+        <br/>
+        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+        <div className='about-explore-container'>
+          <h1>Your destination is waiting. <br/> Your van is ready.</h1>
+          <a>Explore our vans</a>
+        </div>
       </div>
 
       <Footer/>
