306d0aa2a5d1f3e42fd5e7590cb5d3b0eff46905
cruzrafaelx <rafaelcruz@outlook.de>
2024-10-29 05:29:54 +0100

    add Vans page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import {useState, useEffect} from "react";
+import './App.css';
+import './server.js';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -7,17 +9,52 @@ import About from './Components/About';
 import Vans from './Components/Vans';
 
 
-import "./server"
-
-
 function App() {
+
+
+  //useState to store the van data from mirage server
+  const [vanList, setVanList] = useState([])
+  // const [error, setError] = useState(null)
+
+
+  //useEffect to fetch data from server
+  useEffect(()=> {
+
+    const fetchData = async () => {
+
+      try{
+        const response = await fetch("api/vans")
+        if(!response.ok){
+          throw new Error("Network response was not ok")
+        }
+        const result = await response.json()
+        setVanList(result.vans)
+      }
+
+      catch(error){
+        console.log(error)
+      }
+
+      //you can also add a finally block if you want to add a loading function
+
+    }
+
+    fetchData()
+
+  }, [])
+
+  console.log(vanList)
+
+
+
+ 
   return (
     <BrowserRouter>
       <Header/>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />}/>
-        <Route path="/vans" element={<Vans />}></Route>
+        <Route path="/vans" element={<Vans vanList={vanList}/>}></Route>
       </Routes>
       <Footer/>
   </BrowserRouter>
--- src/Components/Vans.jsx
+++ src/Components/Vans.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
 
-function Vans() {
+function Vans({vanList}) {
+
+const vans =  vanList.map( van => { 
+
+    const btn = 
+    van.type === "simple" ? "simple" :
+     van.type === "rugged" ? "rugged" : "luxury"
+         
+
+    return(
+    <div key={van.id} className="van-card">
+        <img className="van-img" src={van.imageUrl} alt={`the van ${van.name}`}/>
+        <div className="van-desc-container">
+            <div className="van-desc-top">
+                <p className="van-name">{van.name}</p>
+                <p className="van-price">€{van.price}/day</p>
+            </div>
+            <button className={`van-desc-btn ${btn}`}>{van.type.charAt(0).toUpperCase() + van.type.slice(1)}</button>
+        </div>
+    </div>
+    )
+})
+
+
   return (
-    <div>
-        <h1>Vans go here!</h1>
+    <div className="vans-container">
+        <h1>Explore our van options</h1>
+        <div className="filter-btns">
+            <button className="filter-btn">Simple</button>
+            <button className="filter-btn">Luxury</button>
+            <button className="filter-btn">Rugged</button>
+            <button className="clear-btn">Clear filters</button>
+            
+        </div>
+        <div className="vans-list-container">
+            {vans}
+        </div> 
     </div>
   )
 }
